7a94ced3d2a61a3a63dd7ff62bb3a6e0801b661e
NimaAram1 <aramnima50@gmail.com>
2021-04-27 10:07:50 +0430

    add requirements and creating resturant app

---
+++ resturant/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ResturantConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'resturant'
+    verbose_name = 'رستوران'
